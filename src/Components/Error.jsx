3a3fd21618a1cc1e68f344a32ce836b47f3b984b
Akshara Reddy <aksharavooturi25@gmail.com>
2025-10-28 11:07:20 +0530

    error page created

---
+++ src/Components/Error.jsx
@@ -0,0 +1,13 @@
+import { useRouteError } from "react-router-dom"
+
+export const Error=()=>{
+    const error=useRouteError();
+    return(
+        <>
+            <div>
+                <h1>Oops! Something went wrong.</h1>
+                <p>{error.statusText || error.message}</p>
+            </div>
+        </>
+    )
+}
